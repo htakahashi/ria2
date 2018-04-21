0681dd4d063f42768b4fb6c990139d6276d8379d
Landon Call <landon@thecallfamily.com>
2018-04-21 00:33:38 -0600

    Clean Up

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -13,11 +13,11 @@
           <span class="image">
             <img src="images/blueDress.jpg" alt="Blue dress" />
           </span>
-          <a href="generic.html">
-            <h2>Blue Dress</h2>
+          <a href="/items/1">
+            <h2>LACE DETAILED MAXI DRESS</h2>
             <div class="content">
               <h4>$36.99</h4>
-              <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+              <p>Move from the beach and beyond in this maxi dress!</p>
               <div class="button special" href="#">Buy Now</div>
             </div>
           </a>
@@ -26,11 +26,11 @@
           <span class="image">
             <img src="images/jeansW.jpg" alt="Women's jeans" />
           </span>
-          <a href="generic.html">
-            <h2>Women's Jeans</h2>
+          <a href="/items/2">
+            <h2>EMBELLISHED SKINNY JEANS</h2>
             <div class="content">
               <h4>$36.99</h4>
-              <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+              <p>Here's the skinny - glam jeans are in this season!</p>
               <div class="button special" href="#">Buy Now</div>
             </div>
           </a>
@@ -39,11 +39,11 @@
           <span class="image">
             <img src="images/blouse.jpg" alt="Blouse" />
           </span>
-          <a href="generic.html">
-            <h2>Blouse</h2>
+          <a href="/items/3">
+            <h2>PLAID LACE UP TOP</h2>
             <div class="content">
               <h4>$39.00</h4>
-              <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+              <p>Classic, plaid print top is no longer borrowed from the boys; with lace up details that requires a cami underneath.</p>
               <div class="button special" href="#">Buy Now</div>
             </div>
           </a>
@@ -52,11 +52,11 @@
           <span class="image">
             <img src="images/suit.jpeg" alt="Suit" />
           </span>
-          <a href="generic.html">
-            <h2>Suit</h2>
+          <a href="/items/4">
+            <h2>STRETCH PERFORMANCE SOLID SLIM-FIT SUIT</h2>
             <div class="content">
               <h4>$595.00</h4>
-              <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+              <p>Perfect for traveling or for staying at your home base, this slim fit black suit from Alfani is essential to your professional wardrobe.</p>
               <div class="button special" href="#">Buy Now</div>
             </div>
           </a>
@@ -65,11 +65,11 @@
           <span class="image">
             <img src="images/jeansM.jpeg" alt="Men's jeans" />
           </span>
-          <a href="generic.html">
-            <h2>Men's Jeans</h2>
+          <a href="/items/5">
+            <h2>541™ Athletic Fit Jeans</h2>
             <div class="content">
               <h4>$69.50</h4>
-              <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+              <p>Athletic fit jeans are roomier in the seat and thigh, with a slightly tapered leg opening and added stretch for a comfortable fit.</p>
               <div class="button special" href="#">Buy Now</div>
             </div>
           </a>
@@ -78,11 +78,11 @@
           <span class="image">
             <img src="images/shirt.jpeg" alt="Shirt" />
           </span>
-          <a href="generic.html">
-            <h2>Shirt</h2>
+          <a href="/items/6">
+            <h2>Classic-Fit Supima® Blend Cotton Polo</h2>
             <div class="content">
-              <h4>$14.99</h4>
-              <p>Sed nisl arcu euismod sit amet nisi lorem etiam dolor veroeros et feugiat.</p>
+              <h4>$24.99</h4>
+              <p>Refine your classic sporty look with the comfortable fit and clean design of this polo from Tasso Elba.</p>
               <div class="button special" href="#">Buy Now</div>
             </div>
           </a>
