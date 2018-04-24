c6460f174c1f6fdc65d3f3752f956c56af41d327
Branton Harper <brantonharper@gmail.com>
2018-04-23 22:12:04 -0600

    Fixed folder structure
    Uploaded an extra folder instead of replacing files in already created folders

--- app/User.php
+++ app/User.php
@@ -4,10 +4,15 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -26,4 +31,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    $admin = User::role('admin')->get(); 
 }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,43 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/customer', function () {
+    return view('customer');
+});
+
+Route::get('/products', function () {
+    return view('products');
+});
+
+Route::get('/thankyou', function () {
+    return view('thankyou');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/orders', 'HomeController@orders')->name('home');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+// resources
+
+Route::resource('users', 'UserController');
+Route::resource('roles', 'RoleController');
+Route::resource('posts', 'PostController');
+Route::resource('permissions','PermissionController');
+
