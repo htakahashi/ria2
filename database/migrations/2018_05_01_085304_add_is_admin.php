46e8acce03038d86d9b7003395837bcddf3785f0
Jeremy Ashby <jeremyashby25@gmail.com>
2018-05-01 04:14:01 -0600

    Uploading Final

---
+++ database/migrations/2018_05_01_085304_add_is_admin.php
@@ -0,0 +1,29 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class AddIsAdmin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       Schema::table('users', function($table) {
+         $table->integer('isAdmin');
+       });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       Schema::table('users', function($table) {
+         $table->dropColumn('isAdmin');
+       });
+    }
+}
